ecdcf80fa14970ca74675ae11b4f9a3d4d6e709e
Amanda Chan <amandac-x@hotmail.com>
2017-09-22 07:07:18 +0800

    Added new start point touch function, moving functionalities for the robot display, merging of layout and setting and removing of obstacles.

--- app/src/main/java/com/mdp/MainActivity.java
+++ app/src/main/java/com/mdp/MainActivity.java
@@ -10,7 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private TextView connect_text;
+
     private boolean hasBT = false;
 
     private Button forward_btn;
@@ -31,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
     private static String command_forward = "f";
     private static String command_right = "tr";
     private static String command_left = "tl";
+
+    private MapHandler mapHandler;
 
     private BroadcastReceiver bluetoothReceiver = new BroadcastReceiver() {
         @Override
@@ -56,26 +62,106 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "create: MainActivity");
 
         //define variables
+        final GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+
+        final GridView mapgridview = (GridView) findViewById(R.id.mapgridview);
+        mapgridview.setAdapter(new MapImageAdapter(this));
+        mapHandler = new MapHandler(gridview, mapgridview, this);
+
+        Button test = (Button) findViewById(R.id.btn2);
+        test.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int x = Integer.parseInt(((EditText)findViewById(R.id.et1)).getText().toString());
+                int y = Integer.parseInt(((EditText)findViewById(R.id.et2)).getText().toString());
+                mapHandler.setStartPoint(x, y);
+            }
+        });
+
         connect_text = (TextView) findViewById(R.id.connect_text);
 
         forward_btn = (Button) findViewById(R.id.btn_top);
         forward_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                sendCommand(command_forward);
+                mapHandler.moveFront();
             }
         });
         left_btn = (Button) findViewById(R.id.btn_left);
         left_btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                sendCommand(command_left);
+            public void onClick(View v) {mapHandler.setDirection(mapHandler.LEFT);
             }
         });
         right_btn = (Button) findViewById(R.id.btn_right);
         right_btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                sendCommand(command_right);
+            public void onClick(View v) { mapHandler.setDirection(mapHandler.RIGHT);
             }
         });
+
+        Button set_obs = (Button) findViewById(R.id.set_obs);
+        set_obs.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int x = Integer.parseInt(((EditText)findViewById(R.id.obsx)).getText().toString());
+                int y = Integer.parseInt(((EditText)findViewById(R.id.obsy)).getText().toString());
+                mapHandler.setObs(x,y);
+            }
+        });
+
+        Button rmv_obs = (Button) findViewById(R.id.rmv_obs);
+        rmv_obs.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int x = Integer.parseInt(((EditText)findViewById(R.id.obsx)).getText().toString());
+                int y = Integer.parseInt(((EditText)findViewById(R.id.obsy)).getText().toString());
+                mapHandler.removeObs(x,y);
+            }
+        });
+
+        final Button setwp_btn = (Button)findViewById(R.id.set_wp);
+        setwp_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(setwp_btn.isActivated()){
+                    setwp_btn.setText("Set WP");
+                    setwp_btn.setActivated(false);
+                    mapgridview.setOnItemClickListener(null);
+                }else{
+                    setwp_btn.setText("cancel");
+                    setwp_btn.setActivated(true);
+                    mapgridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                            int[] coord = mapHandler.getCoordinates(position);
+                            mapHandler.alertView(setwp_btn, "Confirm set way point?", "Are you sure that you want to set \n " +
+                                    "the point ("+coord[0]+", "+coord[1]+") as your way point?",true,mapHandler.SETWP);
+                            mapHandler.poswp = position;
+                        }
+                    });
+                }
+            }
+        });
+
+        final Button setsp_btn = (Button)findViewById(R.id.set_sp);
+        setsp_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(setsp_btn.isActivated()){
+                    setsp_btn.setText("Set Start");
+                    setsp_btn.setActivated(false);
+                    mapgridview.setOnItemClickListener(null);
+                }else{
+                    setsp_btn.setText("cancel");
+                    setsp_btn.setActivated(true);
+                    mapgridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                            int[] coord = mapHandler.getCoordinates(position);
+                            mapHandler.alertView(setsp_btn, "Confirm set start point?", "Are you sure that you want to set \n " +
+                                    "the point ("+coord[0]+", "+coord[1]+") as your start point?",true,mapHandler.SETSP);
+                            mapHandler.possp = position;
+                        }
+                    });
+                }
+            }
+        });
+
     }
 
     public void sendCommand(String s){
